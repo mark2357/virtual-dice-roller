88ec73cced892a2111198b0c01b66c05fb7701d9
mark2357 <marklenton42@gmail.com>
2020-04-07 20:58:39 +1000

    service worker path bug fix, version 0.2.1

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ library.add(faChevronLeft, faChevronRight, faChevronUp, faChevronDown, faTimes, 
 //setup service worker
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-        navigator.serviceWorker.register('/service-worker.js').then(registration => {
+        navigator.serviceWorker.register('./service-worker.js').then(registration => {
             console.log('SW registered: ', registration);
         }).catch(registrationError => {
             console.log('SW registration failed: ', registrationError);
